7e4d281d8d023576e5dc46ab8d3a2e04b5c18fa3
Mohammad222PR <mohammades13851@gmail.com>
2023-08-29 20:16:18 +0330

    new commit5

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
-from account.forms import LoginForm
+from account.forms import LoginForm, SingupForm
 
 
 # Create your views here.
@@ -26,21 +26,14 @@ def Register(request):
     context = {"errors": []}
     if request.user.is_authenticated:
         return redirect('home:home')
-
     if request.method == "POST":
-        username = request.POST['username']
-        email = request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-        if password1 != password2:
-            context['errors'].append('password not mach')
-            return render(request, 'account/register.html', context)
-
-        User.objects.create_user(username=username, email=email, password=password1)
-        user = authenticate(request, username=username, password=password1)
-        login(request, user)
-        return redirect('home:home')
-
+        form = SingupForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['password1'], cd['password2'])
+            user = authenticate(request, username=cd['username'], password=cd['password1'])
+            login(request, user)
+            return redirect('home:home')
     return render(request, 'account/register.html', {})
 
 
